8091ae01182c6a2775ae01a852c5c7a317347126
tonyxuan2021 <tonyyexuan@gmail.com>
2022-07-15 15:10:30 -0700

    fix category page not adding to cart issue

--- src/components/Category/Horror.js
+++ src/components/Category/Horror.js
@@ -1,55 +1,65 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./Genre.scss";
 import uniqid from "uniqid";
 import axios from "axios";
 import Loader from "../Loader/Loader";
+import CartContext from "../../CartContext";
 
-class Horror extends Component {
-  state = {
-    bookDataAdventure: [],
-  };
+const Horror = () => {
+  const [bookDataAdventure, setBookDataAdventure] = useState([]);
 
-  componentDidMount() {
+  const { addToCart } = useContext(CartContext);
+
+  useEffect(() => {
     axios
       .get("https://www.googleapis.com/books/v1/volumes?q=horror")
       .then((response) => {
-        this.setState({
-          bookDataAdventure: response.data.items,
-        });
+        setBookDataAdventure(response.data.items);
       });
-  }
+  }, []);
 
-  render() {
-    if (this.state.bookDataAdventure.length === 0) {
-      return (
-        <section>
-          <Loader>Loading...</Loader>
-        </section>
-      );
-    }
-
+  if (bookDataAdventure.length === 0) {
     return (
-      <div className="genre__wrapper">
-        {this.state.bookDataAdventure.map((bookObj) => {
-          return (
-            <div className="genre__card__wrapper" key={uniqid()}>
-              <img
-                src={
-                  bookObj.volumeInfo.imageLinks?.smallThumbnail ||
-                  bookObj.volumeInfo.imageLinks?.thumbnail
-                }
-                className="genre__card__img"
-              ></img>
-              <h3>{bookObj.volumeInfo.title}</h3>
-              <p>{bookObj.volumeInfo.authors}</p>
-              <h3>{`$ ${19.99}`}</h3>
-              <button className="genre__btn">Add to cart</button>
-            </div>
-          );
-        })}
-      </div>
+      <section>
+        <Loader>Loading...</Loader>
+      </section>
     );
   }
-}
+
+  return (
+    <div className="genre__wrapper">
+      {bookDataAdventure.map((bookObj) => {
+        const { volumeInfo } = bookObj;
+        return (
+          <div className="genre__card__wrapper" key={uniqid()}>
+            <img
+              src={
+                bookObj.volumeInfo.imageLinks?.smallThumbnail ||
+                bookObj.volumeInfo.imageLinks?.thumbnail
+              }
+              className="genre__card__img"
+            ></img>
+            <h3>{bookObj.volumeInfo.title}</h3>
+            <p>{bookObj.volumeInfo.authors}</p>
+            <h3>{`$ ${19.99}`}</h3>
+            <button
+              onClick={() => {
+                addToCart(
+                  volumeInfo.title,
+                  19.99,
+                  volumeInfo.imageLinks.smallThumbnail,
+                  volumeInfo.authors[0] || ""
+                );
+              }}
+              className="genre__btn"
+            >
+              Add to cart
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Horror;
