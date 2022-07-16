ac3c822ef6dde7ab2f5385f8ca71acab650ba109
tonyxuan2021 <tonyyexuan@gmail.com>
2022-07-15 17:27:15 -0700

    complete site redesign

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,74 +8,96 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { Link, useHistory } from "react-router-dom";
 import CartContext from "../../CartContext";
 import axios from "axios";
+import Slider from "../Slider/Slider";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button, TextField, InputProps } from "@mui/material";
+import { Box } from "@mui/system";
 
 const Header = () => {
   const { items } = useContext(CartContext);
   const [booksData, setBooksData] = useState([]);
+  const [search, setSearch] = useState("");
 
   let history = useHistory();
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    const search = e.target.search.value;
-    // console.log(search);
     axios
       .get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${search}`)
       .then((data) => {
-        console.log(data);
         setBooksData(data);
         history.push(`/search/${search}`);
       });
-
     e.target.reset();
   };
 
   return (
     <div>
-      <div className="header__wrapper">
-        <div className="header--top">
-          <Link to="/" className="header__link">
-            <img src={logo} className="header__img"></img>
-          </Link>
-          <div className="header--right__wrapper">
-            <div className="header--top">
-              <p>Login</p>
-              <FontAwesomeIcon icon={faUser} />
-            </div>
-            <div className="header--btm">
-              <Link to="/checkout" className="header--right">
-                <p>Cart</p>
-                <FontAwesomeIcon
-                  icon={faCartShopping}
-                  className="header__cart"
-                />
-                <span className="header__cart__num">{items.length}</span>
-              </Link>
+      <div className="header__final">
+        <div className="header__wrapper">
+          <div className="header--top">
+            <Link to="/" className="header__link">
+              <img src={logo} className="header__img"></img>
+            </Link>
+            <div className="header--right__wrapper">
+              <div className="header--top">
+                <p>Login</p>
+                <FontAwesomeIcon icon={faUser} />
+              </div>
+              <div className="header--btm">
+                <Link to="/checkout" className="header--right">
+                  <p>Cart</p>
+                  <FontAwesomeIcon
+                    icon={faCartShopping}
+                    className="header__cart"
+                  />
+                  <span className="header__cart__num">{items.length}</span>
+                </Link>
+              </div>
             </div>
           </div>
+          <div className="header__bar">
+            <Link to="/" className="header__home">
+              <p>Home</p>
+            </Link>
+            <Link to="/used" className="header__used">
+              <p>Sell Used</p>
+            </Link>
+          </div>
         </div>
-        <div className="header__bar">
-          <Link to="/" className="header__home">
-            <p>Home</p>
-          </Link>
-          <Link to="/used" className="header__used">
-            <p>Sell Used</p>
-          </Link>
-        </div>
-        <form className="search__form__wrapper" onSubmit={handleSubmit}>
-          <input
-            className="search__input"
-            placeholder="Search over millions of books..."
-            name="search"
-          ></input>
-          <button className="header__search" onSubmit={handleSubmit}>
-            Search
-          </button>
-        </form>
+        <Box sx={{ height: 45, mb: 2 }} display="flex" className="header__box">
+          <TextField
+            placeholder="Search over millions of books"
+            variant="outlined"
+            value={search}
+            autoFocus
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            // InputProps={{ style: { fontSize: 16 } }}
+            sx={{ width: "70%" }}
+          ></TextField>
+          <Button
+            className="header__search__btn"
+            onClick={handleSubmit}
+            variant="contained"
+            endIcon={
+              <SearchIcon
+                sx={{
+                  color: "black",
+                  transform: "scale(1.2)",
+                }}
+              />
+            }
+            sx={{
+              width: "30%",
+              height: "100%",
+              background: "#b5e48c",
+              "&:hover": { background: "#C7F9CC" },
+            }}
+          ></Button>
+        </Box>
       </div>
-      <div className="header__banner">
-        <p className="header__banner__text">FREE shipping on orders over $50</p>
-      </div>
+      <Slider />
     </div>
   );
 };
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,43 @@
+import { Navigation, Autoplay } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+const styles = {
+  center: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    background: "grey",
+    color: "white",
+    height: "4rem",
+  },
+};
+
+export default () => {
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Autoplay]}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      navigation
+      style={{marginBottom: "1.6rem"}}
+    >
+      <SwiperSlide style={styles.center}>
+        FREE shipping on orders over $50
+      </SwiperSlide>
+      <SwiperSlide style={styles.center}>
+        Buy 2 any Books, Get the 3rd Free.
+      </SwiperSlide>
+      <SwiperSlide style={styles.center}>
+        Upgrade to Booktown member $99/year and get a book Free!
+      </SwiperSlide>
+    </Swiper>
+  );
+};
